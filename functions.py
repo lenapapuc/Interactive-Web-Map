6d19ab32d8af58e817559c7841d40ed34b4ea527
lenapapuc <elenapapuc99@gmail.com>
2024-07-31 20:37:15 +0300

    Changed dataset from USA to world volcanoes, added legend

--- functions.py
+++ functions.py
@@ -1,11 +1,20 @@
-def elevation_calculation(elevation):
+def elevation_calculation(el):
     color = ""
+
+    try:
+        elevation = int(el)
+    except ValueError:
+        return "blue"
+    
+    elevation = int(el)
     if elevation <= 2000:
         color = "green"
     elif elevation <=3000:
         color = "orange"
     elif elevation > 3000:
         color = "red"
+    else:
+        color = "blue"
     
     return color
   
--- map1.py
+++ map1.py
@@ -2,18 +2,18 @@ import folium
 import pandas as pd
 import xyzservices.providers as xyz
 import functions
-
+import branca
 # Reading the volcanoes file and extracting the necessary details
-data = pd.read_csv("volcanoes.txt")
-lat = list(data["LAT"])
-lon = list(data["LON"])
-name = list(data["NAME"])
-elevation = list(data["ELEV"])
+data = pd.read_csv("volcanoes2.csv")
+lat = list(data["Latitude"])
+lon = list(data["Longitude"])
+name = list(data["Volcano Name"])
+elevation = list(data["Elevation (m)"])
 
 html = """<h4>Volcano information:</h4>
 Name: %s \n
 
-Height: %s m 
+Height in meters: %s 
 """
 
 # Using xyz library, I got a custom set of tiles
@@ -22,20 +22,50 @@ tiles = xyz.Stadia.StamenTerrain.build_url()
 attr =  xyz.Stadia.StamenTerrain.html_attribution
 
 # Creating a feature group (important for layers)
-feature_group = folium.FeatureGroup(name="Markers")
+feature_group = folium.FeatureGroup(name = "Volcanoes")
+feature_group1 = folium.FeatureGroup(name = "Countries Population")
 
 # Adding each marker to the map
 for lt, lg, nm, el in zip(lat, lon, name, elevation):
     iframe = folium.IFrame(html=html % (nm, str(el)), width=200, height=100)
-    feature_group.add_child(folium.CircleMarker(location=[lt, lg], radius = 10,
+    feature_group.add_child(folium.CircleMarker(location=[lt, lg], radius = 6   ,
                                                  popup= folium.Popup(iframe),
                                                  color = "black", 
                                                  weight = 1,
                                                  fill_opacity = 1,
                                                  fill_color=str(functions.elevation_calculation(el))))
 
+feature_group1.add_child(folium.GeoJson(data = open("world.json", 'r', encoding="utf-8-sig").read(), 
+                                       style_function=lambda x: {'fillColor':'green' if x['properties']['POP2005'] < 10000000
+                                                                 else 'orange' if 10000000 <= x['properties']['POP2005'] < 20000000 else 'red'}, 
+                                                                 popup=folium.GeoJsonPopup(fields=["POP2005"], labels=False),
+                                                                 zoom_on_click=True))
 # Create a map with the specified tiles and attribution
 map = folium.Map(location=[38.58, -99], tiles=tiles, attr=attr, zoom_start=4)
+
+legend_html = '''
+{% macro html(this, kwargs) %}
+<div style="position: fixed; 
+     bottom: 50px; left: 50px; width: 250px; height: 150px; 
+     border:2px solid grey; z-index:9999; font-size:14px;
+     background-color:white; opacity: 0.85;">
+     &nbsp; <b>Legend</b> <br>
+     &nbsp; Unknown Height &nbsp; <i class="fa fa-circle" style="color:blue;"></i><br>
+     &nbsp; Height less than 2000 m &nbsp; <i class="fa fa-circle" style="color:green; border-color: black"></i><br>
+     &nbsp; Height between 2000 and 3000 m &nbsp; <i class="fa fa-circle" style="color:orange; border-color: black"></i><br>
+     &nbsp; Height more than 3000 m &nbsp; <i class="fa fa-circle" style="color:red; border-color: black"></i><br>
+</div>
+{% endmacro %}
+'''
+
+legend = branca.element.MacroElement()
+legend._template = branca.element.Template(legend_html)
+
+map.add_child(feature_group1)
+
 map.add_child(feature_group)
 
+map.add_child(folium.LayerControl())
+
+map.get_root().add_child(legend)
 map.save("map1.html")
